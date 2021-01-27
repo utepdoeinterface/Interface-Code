86a3692c2e63fec5cf473d8ee583ea5321ad1030
utepdoe <utepdoe@gmail.com>
2021-01-26 17:36:08 -0700

    Updates

---
+++ precache-manifest.e0f57e787798c3c0318b0b81106d7afd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5a391913f60462576e6ebdd89e429d",
+    "url": "/Interface-Code/index.html"
+  },
+  {
+    "revision": "c50b292e98a5405809b0",
+    "url": "/Interface-Code/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c50b292e98a5405809b0",
+    "url": "/Interface-Code/static/js/2.8bca49ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Interface-Code/static/js/2.8bca49ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41fce00c9ebe277cb24",
+    "url": "/Interface-Code/static/js/main.69284627.chunk.js"
+  },
+  {
+    "revision": "b4bb1e95815bd96258d3",
+    "url": "/Interface-Code/static/js/runtime-main.9a4d55a2.js"
+  }
+]);
